c7b97458d47542d7726512e68e528f48e038c248
陈丹霞 <chendanxia@xylink.com>
2019-08-21 17:32:57 +0800

    o

--- data/parse_data.py
+++ data/parse_data.py
@@ -9,12 +9,16 @@ img_dir=os.path.join(base_dir,'imagenet_train')
 bbox_path=os.path.join(base_dir,'bbox_infos.npy')
 dst_img_dir='/data/sophie_bak/imagenet_train_crop'
 bbox_dict=np.load(bbox_path).item()
-for dir_name in tqdm(os.listdir(img_dir)):
+dirs=os.listdir(img_dir)
+dirs.sort()
+for dir_name in tqdm(dirs):
     dir_path = os.path.join(img_dir,dir_name)
     dst_dir=os.path.join(dst_img_dir,dir_name)
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
-    for file_name in os.listdir(dir_path):
+    subdirs=os.listdir(dir_path)
+    subdirs.sort()
+    for file_name in subdirs:
         file_path = os.path.join(dir_path,file_name)
         clear_file_name=file_name.split('.')[0]
         bbox_info=bbox_dict.get(clear_file_name)

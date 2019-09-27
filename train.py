a6cc1a9460b92b2e2f653591259508a919006750
陈丹霞 <chendanxia@xylink.com>
2019-09-27 15:18:43 +0800

    fix

--- train.py
+++ train.py
@@ -30,22 +30,9 @@ def parseArguments():
 
 args=parseArguments()
 
-def main():
-    is_training=True
-    batch_norm_params = {
-        'decay': 0.995,
-        'epsilon': 0.001,
-        'updates_collections': None,
-        'variables_collections': [ tf.GraphKeys.TRAINABLE_VARIABLES ],
-    }
-    
-    #init input
-    with tf.variable_scope('input'):
-        x_pl=tf.placeholder(dtype=tf.uint8, shape=[args.batch_size, args.height, args.width, 3], name='x_pl')
-        y_label=tf.placeholder(dtype=tf.int32, shape=[args.batch_size], name='y_label')
-    
+def inference(x_batch, is_training):
     #build the network
-    encoder = ShuffleNet(x_pl, num_classes=2, pretrained_path="", train_flag=is_training, weight_decay=args.weight_decay)
+    encoder = ShuffleNet(x_batch, num_classes=2, pretrained_path="", train_flag=is_training, weight_decay=args.weight_decay)
     encoder.build()
     net = slim.flatten(encoder.stage4)
     net = slim.dropout(net, args.dropout, is_training=is_training)
@@ -58,12 +45,34 @@ def main():
     cross_entropy_loss=tf.reduce_mean(ce)
     regularization_loss = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
     loss=cross_entropy_loss+regularization_loss
-    saver = tf.train.Saver(max_to_keep=1)
-    #start the train
+    
+    return pre_class, loss
+    
+def main():
+    is_training=True
+    batch_norm_params = {
+        'decay': 0.995,
+        'epsilon': 0.001,
+        'updates_collections': None,
+        'variables_collections': [ tf.GraphKeys.TRAINABLE_VARIABLES ],
+    }
+    
+    #init input
+#     with tf.variable_scope('input'):
+#         x_pl=tf.placeholder(dtype=tf.uint8, shape=[args.batch_size, args.height, args.width, 3], name='x_pl')
+#         y_label=tf.placeholder(dtype=tf.int32, shape=[args.batch_size], name='y_label')
+    learning_rate_placeholder = tf.placeholder(dtype=tf.float32, name='learning_rate')
     dataset, batch_num_one_epoch=get_dataset(args)
     iterator=dataset.make_initializable_iterator()
-    learning_rate=5e4
+    x_batch, y_batch= iterator.get_next()
+    x_batch.set_shape((args.batch_size, args.height, args.width, 3))
+    y_batch.set_shape((args.batch_size,))
+    
+    pre_class, loss = inference(x_batch, is_training)
+    saver = tf.train.Saver(max_to_keep=1)
+    #start the train
     global_step=tf.train.get_or_create_global_step()
+    train_op=get_train_op(loss, global_step, learning_rate_placeholder)
     with tf.Session() as sess:
         save_path=tf.train.latest_checkpoint(args.checkpoint_dir)
         if save_path != None:
@@ -72,24 +81,22 @@ def main():
         for i in range(args.num_epochs):
             sess.run(iterator.initializer)
             learning_rate=misc.get_learning_rate(args.learning_rate_file, i)
-            train_op=get_train_op(loss, global_step, learning_rate)
+            
             for i in tqdm(range(batch_num_one_epoch)):
-                x_batch, y_batch= iterator.get_next()
-                x_batch.set_shape(x_pl.get_shape())
-                y_batch.set_shape(y_label.get_shape())
-                sess.run(train_op, feed_dict={x_pl:x_batch, y_label:y_batch})
+                sess.run(train_op, feed_dict={learning_rate_placeholder:learning_rate})
             if i % args.validate_every == 0:
-                validate(pre_class)
+                validate(sess)
             if i % args.save_every == 0:
                 saver.save(sess, args.checkpoint_dir, global_step)
                 
-def validate(pre_class):
+def validate(sess):
     dataset, batch_num = get_val_dataset(args)
     labels=[]
     pres=[]
+    x_batch, y_batch = iterator.get_next()
+    pre_class, _ = inference(x_batch, False)
     for i in tqdm(range(batch_num)):
-        x_batch, y_batch = iterator.get_next()
-        pre = sess.run(pre_class, feed_dict={x_pl:x_batch, y_label:y_batch})
+        pre = sess.run(pre_class)
         pres.extend(pre)
         labels.extend(y_batch)
     tp=np.sum(np.where(lables-pres==0,1,0))

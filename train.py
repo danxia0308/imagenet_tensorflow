5a62d69950016737adb76f74f50686b9a0f96607
陈丹霞 <chendanxia@xylink.com>
2019-10-08 16:33:58 +0800

    debug

--- train.py
+++ train.py
@@ -39,9 +39,9 @@ def inference(x_batch, y_batch, is_training):
     encoder = ShuffleNet(x_batch, num_classes=2, pretrained_path="", train_flag=is_training, weight_decay=args.weight_decay)
     encoder.build()
     net = slim.flatten(encoder.stage4)
-    print(net.op.name, net.shape.as_list())
+    print(net,net.op.name, net.shape.as_list())
     net = slim.dropout(net, args.dropout, is_training=is_training, scope='Dropout')
-    print(net.op.name, net.shape.as_list())
+    print(net,net.op.name, net.shape.as_list())
     #weights_initializer=slim.l2_regularizer(args.weight_decay)
     batch_norm_params = {
         'decay': 0.995,
@@ -51,7 +51,7 @@ def inference(x_batch, y_batch, is_training):
     }
     net=slim.fully_connected(net, 1000, normalizer_fn=slim.batch_norm, normalizer_params=batch_norm_params, 
                          trainable=is_training, scope='Bottleneck')#, reuse=False)
-    print(net.op.name, net.shape.as_list())
+    print(net,net.op.name, net.shape.as_list())
 #     net = slim.fully_connected(net, bottleneck_layer_size, activation_fn=None, 
 #                         scope='Bottleneck', reuse=False)
 #     weight = slim.model_variable('fc_weight', shape = [FLAGS.embedding_size, nrof_classes], regularizer = slim.l2_regularizer(FLAGS.weight_decay), initializer = tf.contrib.layers.xavier_initializer(uniform=False), device='/cpu:0')

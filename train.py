386ba3c541d8bdb192bf48ad8c90eda3e46f89fd
陈丹霞 <chendanxia@xylink.com>
2019-10-08 17:57:27 +0800

    f

--- train.py
+++ train.py
@@ -196,7 +196,7 @@ def get_val_dataset(args):
     with open(args.val_label_file) as f:
         content=f.read()
         labels=content.split('\n')[:-1]
-    pdb.set_trace()
+#     pdb.set_trace()
     names=os.listdir(args.val_img_dir)
     names.sort()
     dataset = tf.data.Dataset.from_tensor_slices((names,labels))

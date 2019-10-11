32bba2dc958f64ffa3e6a3aa7590ada1d23ecf14
陈丹霞 <chendanxia@xylink.com>
2019-10-11 18:29:25 +0800

    f

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def main():
     #start the train
     global_step=tf.train.get_or_create_global_step()
     optimizer = tf.train.AdamOptimizer(learning_rate_placeholder)
-    with tf.variable_scope(tf.get_variable_scope()):
+    with tf.variable_scope(tf.get_variable_scope()) as scope:
         tf.get_variable_scope().reuse_variables()
         pred_class,  net= inference(x_batch, train_placeholder)
 #         tf.get_variable_scope().reuse_variables()

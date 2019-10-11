ffa009b8af4a8b69202fa374f6bf5e70194f20e1
陈丹霞 <chendanxia@xylink.com>
2019-10-11 16:46:14 +0800

    f

--- train.py
+++ train.py
@@ -103,11 +103,9 @@ def main():
     global_step=tf.train.get_or_create_global_step()
     optimizer = tf.train.AdamOptimizer(learning_rate_placeholder)
     with tf.variable_scope(tf.get_variable_scope()):
-        with tf.name_scope("tower0") as scope:
-            with slim.arg_scope([slim.model_variable, slim.variable], device='/cpu:0'):
-                pred_class,  net= inference(x_batch, train_placeholder)
-                tf.get_variable_scope().reuse_variables()
-                pred_class_test, _ = inference(test_input_placeholder, train_placeholder)
+        pred_class,  net= inference(x_batch, train_placeholder)
+        tf.get_variable_scope().reuse_variables()
+        pred_class_test, _ = inference(test_input_placeholder, train_placeholder)
     match_result=tf.equal(pred_class, tf.cast(y_batch,tf.int64))
     match_sum=tf.reduce_sum(tf.cast(match_result,tf.float32))
     acc=match_sum/args.batch_size
